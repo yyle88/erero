7276b478cbee523e46d8a43e016ecdec49833ccf
yangyile <example@gogit.com>
2024-06-29 20:50:45 +0700

    增加erero.Is函数

--- erero.go
+++ erero.go
@@ -44,3 +44,8 @@ func Errorf(format string, args ...interface{}) error {
 	}
 	return erx
 }
+
+// Is 就是判定错误是否和另一个错误相等的，把这个函数也写过来，能避免引用 errors 包避免包名冲突还要使用别名
+func Is(err, target error) bool {
+	return errors.Is(err, target)
+}
